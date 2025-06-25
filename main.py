6452c6d7aa4448c7c131a7af5e4c2e8601f5612d
eduardo almada <almada.eduardo@gmail.com>
2025-06-25 12:35:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-from flask import Flask
-from dotenv import load_dotenv
-import os
-from app.tasks import tarefa_teste
+from app import create_app
 
-load_dotenv()
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Recorte Digital OABRJ rodando em produção!"
-
-@app.route("/testar-celery")
-def testar_celery():
-    tarefa_teste.delay()
-    return "✅ Tarefa Celery enviada para o worker"
+app = create_app()
