c39df01a1d6d2c76ad3a319dc08b579cc077bdd4
eduardo almada <almada.eduardo@gmail.com>
2025-06-25 15:41:40 -0300

    Update main.py
    volta do código APP com o ping

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+from app.tasks import tarefa_teste
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Recorte Digital OABRJ rodando em produção!"
+
 @app.route("/ping")
 def ping():
     return "pong"
+
+@app.route("/testar-celery")
+def testar_celery():
+    tarefa_teste.delay()
+    return "✅ Tarefa Celery enviada para o worker"
