8ae1472a06dae7ce5bd8e836f97cc4ef69e55bf0
eduardo almada <almada.eduardo@gmail.com>
2025-07-02 17:27:17 -0300

    Update models.py
    Definicao dos novos modelos

--- app/models.py
+++ app/models.py
@@ -1,26 +1,20 @@
-from .db import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class Advogado(db.Model):
     __tablename__ = 'advogados'
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String)
-    oab = db.Column(db.String)
-    telefone = db.Column(db.String)
+    nome = db.Column(db.String(255), nullable=False)
+    numero_oab = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    whatsapp = db.Column(db.String(20), nullable=False)
 
 class Publicacao(db.Model):
     __tablename__ = 'publicacoes'
     id = db.Column(db.Integer, primary_key=True)
-    nome_citado = db.Column(db.String)
-    oab_citada = db.Column(db.String)
-    data = db.Column(db.Date)
-    conteudo = db.Column(db.Text)
-    link = db.Column(db.String)
-    enviada = db.Column(db.Boolean, default=False)
-
-class LogEnvio(db.Model):
-    __tablename__ = 'logs_envio'
-    id = db.Column(db.Integer, primary_key=True)
-    advogado_id = db.Column(db.Integer)
-    publicacao_id = db.Column(db.Integer)
-    status = db.Column(db.String)
-    horario = db.Column(db.String)
+    advogado_id = db.Column(db.Integer, db.ForeignKey('advogados.id'), nullable=False)
+    titulo = db.Column(db.String(500), nullable=False)
+    descricao = db.Column(db.Text)
+    data_publicacao = db.Column(db.Date)
+    link = db.Column(db.String(1000))
